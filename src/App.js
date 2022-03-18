a9cec9f63e7f5f80477f0a0252364235932c9c22
accounts@shawnpatoka.com <accounts@shawnpatoka.com>
2022-03-17 21:53:11 -0500

    finished section 4, excl animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
 import FeedbackData from './data/FeedbackData'
+import FeedbackForm from './components/FeedbackForm'
+
 
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const deleteFeedback = (id) => {
+        if(window.confirm('you sure?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+        
+    }
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackList feedback={feedback}/>       
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>       
             </div>
         </>
     )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,19 @@
-function FeedbackItem({item}) {
+import {FaTimes} from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
 
+function FeedbackItem({item, handleDelete}) {
   return (
-    <div className="card">
+    <Card>
         <div className="num-display">{item.rating}</div>
-        <div className="text-desiplay">
-            {item.text}
-        </div>
-    </div>
+        <button onClick={() => handleDelete(item.id)} className='close'><FaTimes color='purple'/></button>
+        <div className="text-desiplay">{item.text}</div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
